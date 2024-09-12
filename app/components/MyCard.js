5682f0b5cf8b3bf17923de130569fc9a8eb7d3b4
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-09-12 08:30:55 -0400

    clean up live code

--- app/components/MyCard.js
+++ app/components/MyCard.js
@@ -1,31 +1,35 @@
+// In order to use react hooks (`useState`, `useEffect`), we must specify that this component runs on the client
 "use client";
-// title -> h2
-// text -> p
-// button text -> button
+// We import react functions
 import { useEffect, useState } from "react";
+// Import the styles from the css file.
 import classes from "./my-card-styles.module.css";
 
 function MyCard(props) {
-  const [lebronCount, setLebronCount] = useState(0);
+  // Creating a React State to keep
+  const [numClicks, setNumClicks] = useState(0);
 
+  // Side effect: we can react to numClicks reaching a certain value
   useEffect(() => {
-    if (lebronCount >= 10) {
-      console.log(lebronCount);
+    if (numClicks >= 10) {
+      // log this message to the browser console (right click + inspect element or F12)
+      console.log("We have clicked more than 10 times!");
     }
-  }, [lebronCount]);
+  }, [numClicks]);
 
-  function increaseCount() {
-    setLebronCount(lebronCount + 1);
+  // Handler function to increase the number of clicks by one
+  function incrementNumClicks() {
+    setNumClicks(numClicks + 1);
   }
 
   return (
     <div className={classes.container}>
-      <h2 className={classes.title}>{props.heading}</h2>
-      <p className={classes.text}>{props.paragraph}</p>
-      <button onClick={increaseCount}>{props.buttonText}</button>
+      <h2 className={classes.title}>{props.title}</h2>
+      <p className={classes.text}>{props.text}</p>
+      <button onClick={incrementNumClicks}>{props.buttonText}</button>
       <p>
-        You clicked lebron <span className={classes.count}>{lebronCount}</span>{" "}
-        times!
+        You clicked the button{" "}
+        <span className={classes.counter}>{numClicks}</span> times!
       </p>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
+// We import components from other files like this
 import MyCard from "./components/MyCard";
 
+// In a `page.js` file, we usually call the page function `Home`
 export default function Home() {
   return (
     <div>
       <p> Hello World!</p>
       <MyCard
-        heading="Heading"
-        paragraph="this is some text that is inside of the card"
-        buttonText="Lebron"
+        // Example of passing in props (properties) to the card
+        title="My React Card"
+        text="This is some text that is inside of the card"
+        buttonText="Click me!"
       ></MyCard>
     </div>
   );
